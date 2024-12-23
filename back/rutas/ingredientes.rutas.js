3fc7b4a47e9e9ebaf0ac41174cb25bd2154b69dd
ezequielastrada <eastrada.in@gmail.com>
2024-12-23 19:58:54 -0300

    ingrediente ruta

---
+++ back/rutas/ingredientes.rutas.js
@@ -0,0 +1,63 @@
+const { Router } = require("express");
+const {getIngredientesService, postIngredientesService, getIngredienteService, updateDateIngredienteService, deleteIngredienteSerivce} = require('../servicios/ingredientes.service')
+
+const routerIngredientes = Router()
+
+routerIngredientes.get('/ingredientes', async (req,res)=>{
+    try{
+        const ingredientes = await getIngredientesService()
+        res.status(200).json(ingredientes);
+    }catch(error){
+        next(error);
+    }
+})
+
+routerIngredientes.post('/ingredientes', async (req,res)=>{
+    try{
+        const datos = req.body
+        console.log(datos)
+        const respuesta = await postIngredientesService(datos)
+        res.status(201).json(respuesta);
+    }catch(error){
+        console.log(error)
+        next(error);
+    }
+})
+
+routerIngredientes.get('/ingredientes/:id', async (req,res)=>{
+    try{
+        console.log(req.params.id)
+        const ingrediente = await getIngredienteService(parseInt(req.params.id))
+        res.status(200).json(ingrediente)
+    }catch(error){
+        console.log(error);
+    }
+
+})
+
+routerIngredientes.put('/ingredientes/:id', async (req, res)=>{
+    try{
+        const id = parseInt(req.params.id)
+        const datos = req.body
+        const respuesta = await updateDateIngredienteService(id,datos)
+        res.status(200).json(respuesta)
+    }catch(error){
+        console.log(error);
+    }
+})
+
+routerIngredientes.delete('/ingredientes/:id', async (req, res)=>{
+    try{
+        const id = parseInt(req.params.id)
+        const respuesta = await deleteIngredienteSerivce(id)
+        res.status(200).json(respuesta)
+    }catch(error){
+        console.log(error);
+        res.status(500).json("No existe tal id")
+    }
+})
+
+
+
+
+module.exports = routerIngredientes
