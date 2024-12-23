3fd0facfcdac4d3619bb64b75eb3e98691b9fd9e
ezequielastrada <eastrada.in@gmail.com>
2024-12-23 19:58:40 -0300

    ingrediente repo

---
+++ back/repositorio/ingredientes.repositorio.js
@@ -0,0 +1,45 @@
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const getIngredientes = async () => {
+    const ingredientes = await prisma.ingrediente.findMany()
+    return ingredientes
+}
+
+const getIngredienteById = async (id) => {
+    const ingrediente = await prisma.ingrediente.findUnique({
+        where: {
+            id: id
+        }
+    })
+    return ingrediente
+}
+
+const postIngrediente = async (datos) => {
+    const ingrediente = await prisma.ingrediente.create({
+        data: datos
+    })
+    return ingrediente;
+}
+
+const updateIngrediente = async (id, datos) => {
+    const ingrediente = await prisma.ingrediente.update({
+        where: {
+            id: id
+        },
+        data: datos
+    })
+    return ingrediente;
+
+}
+
+const deleteIngrdiente = async (id)=> {
+    const respuesta = await prisma.ingrediente.delete({
+        where: {
+            id : id
+        }
+    })
+    return respuesta;
+}
+
+module.exports = {getIngredientes,postIngrediente, getIngredienteById, updateIngrediente, deleteIngrdiente}
