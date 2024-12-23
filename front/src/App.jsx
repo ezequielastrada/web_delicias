837e0ac2b352a7187ed9da6f3572b95fbcbeaf5a
ezequielastrada <eastrada.in@gmail.com>
2024-12-23 20:03:12 -0300

    App.jsx

---
+++ front/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Ingredientes from './componentes/Ingredientes'
+import Tarjeta from './componentes/Tarjeta'
+
+function App() {
+  //<Tabla/>
+  return (
+    <div>
+      <Tarjeta/>
+      <Ingredientes />
+    </div>
+  )
+}
+
+export default App
