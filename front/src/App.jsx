22c988abe0ebc4f0aa2711709cbf988f6da33d80
ezequielastrada <eastrada.in@gmail.com>
2024-12-27 02:11:24 -0300

    add Routes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,31 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-import Ingredientes from './componentes/Ingredientes'
-import Tarjeta from './componentes/Tarjeta'
+import Home from './pages/Home'
+import Ingredientes from './pages/Ingredientes'
+import MisProductos from './pages/MisProductos'
+import FormularioIngrediente from './componentes/FormularioIngrediente';
+
+import './app.css'
 
 function App() {
-  //<Tabla/>
+
   return (
     <div>
-      <Tarjeta/>
-      <Ingredientes />
+      <BrowserRouter>
+      <nav className='app__nav'>
+        <Link to="/">Inicio</Link> | <Link to="/misproductos">Mis Productos</Link> | <Link to="/ingredientes">Materias Primas - Precios</Link>
+      </nav>
+      
+      
+      <div className='app__main'>
+      <Routes >
+        <Route path="/"             element={<Home />} />
+        <Route path="/ingredientes" element={<Ingredientes />} />
+        <Route path="/misproductos" element={<MisProductos />} />
+        <Route path="/nuevo"        element={<FormularioIngrediente />} />
+      </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   )
 }
