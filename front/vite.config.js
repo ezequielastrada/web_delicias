eb9cb109db13ed97bd044b9598531abf8dea65f9
ezequielastrada <eastrada.in@gmail.com>
2024-12-27 02:10:52 -0300

    css modules

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      // Este es el patrón por defecto. Cambiar si deseas personalizarlo
+      localsConvention: 'camelCaseOnly',
+    },
+  }
 })
